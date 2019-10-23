ee0a81f94454abc524407b04ea001f8468b49f67
Brian Shin <bshin9@yahoo.com>
2019-10-23 09:41:59 -0400

    brian branch

--- models/basketball.model.js
+++ models/basketball.model.js
@@ -1,3 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const playerSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3
+    },
+    lastName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3
+    },
+    gender: {
+      type: String,
+      requied: true,
+      trim: true,
+      minlength: 1
+    },
+    age: {
+      type: Number,
+      required: true,
+      trim: true,
+      minlength: 1
+    },
+    position: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1
+    },
+    height: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Player = mongoose.model("player", playerSchema);
+
+module.exports = Player;
--- routes/basketball.js
+++ routes/basketball.js
@@ -1,1 +1,52 @@
-const router = require('express').Router();
+const router = require("express").Router();
+let Player = require("../models/basketball.model");
+
+
+// /players endpoint gets us all player information
+router.get("/players", async (req, res) => {
+  try {
+    const player = await Player.find();
+    res.send(player);
+  } catch {
+    res.send(404);
+  }
+});
+
+// /addplayer lets us add a new player into our database
+router.post("/addplayer", async (req, res) => {
+  try {
+    // we've created a new Player here
+    // then we want to store that information and set it to result
+    let player = new Player(req.body);
+    let result = await player.save();
+
+    res.send(result);
+  } catch {
+    res.send(500);
+  }
+});
+
+// hitting the endpoint to return an update to current data
+router.put("/editplayer/:id", async (req, res) => {
+  try {
+    const player = await Player.findById(req.params.id);
+    player.set(req.body);
+    const result = await player.save();
+    res.send(result);
+  } catch {
+    res.send(404);
+  }
+});
+
+//
+router.delete("/deleteplayer/:id", async (req, res) => {
+  try {
+    const player = await Player.findByIdAndDelete(req.params.id);
+
+    res.send(player);
+  } catch {
+    res.send(404);
+  }
+});
+
+module.exports = router;
