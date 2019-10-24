58a9bd112b20c78592e00747e385c3ad2b1d2ec9
Lovepreet Thind <lovepreetthind@Administrators-MacBook-Pro.local>
2019-10-24 12:24:20 -0400

    Adjusted basketball court picture

--- client/src/component/basketball/BballCourt.jsx
+++ client/src/component/basketball/BballCourt.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BballCourt = () => {   
     return(
         <div>
-            <img src="../images/bbcourt.jpg" />
+            <img src="https://static.vecteezy.com/system/resources/previews/000/182/378/large_2x/basketball-court-sketch-chalkboard-vector.jpg" />
         </div>
     )
 }
